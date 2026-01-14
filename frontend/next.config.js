462d906c7b6f23e7a148b77c5b0191aa332b060b
hkjang <gagagiga@naver.com>
2026-01-14 13:54:54 +0900

    Watchpack Error

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -9,6 +9,14 @@ const nextConfig = {
       },
     ];
   },
+  webpack: (config) => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300,
+      ignored: /node_modules/,
+    };
+    return config;
+  },
 };
 
 module.exports = nextConfig;
